be518dffa0359b2311c3e782ee9d6d2249f699fa
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-09 00:47:32 +0200

    📝 add javascript-basics exercice2.ts

---
+++ javascript-basics/exercice2.ts
@@ -0,0 +1,6 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function myFunction(a: number, b: number): boolean {
+  return a === b;
+}
