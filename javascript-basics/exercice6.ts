498573f599455b9d5cdae31369d4b5d00eb1ec0e
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-09 01:19:33 +0200

    📝 add javascript-basics exercice6.ts

---
+++ javascript-basics/exercice6.ts
@@ -0,0 +1,7 @@
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+
+function myFunction(str: string): string {
+  return str.slice(-3);
+}
