5987177eebda4314143e5847b159ae1d10aa70cd
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-09 01:39:18 +0200

    📝 add javascript-basics solution11.ts

---
+++ javascript-basics/solution11.ts
@@ -0,0 +1,10 @@
+// Write a function that takes 6 values (a,b,c,d,e,f) as arguments
+// Sum a and b
+// Then substract by c
+// Then multiply by d and divide by e
+// Finally raise to the power of f and return the result
+// Tipp: mind the order
+
+function myFunction(a: number, b: number, c: number, d: number, e: number, f: number) {
+  return (((a + b - c) * d) / e) ** f;
+}
