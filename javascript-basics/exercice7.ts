b6212322ce12b43becc18a92e51577a3ee1d9d8b
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-09 01:21:22 +0200

    📝 add javascript-basics exercice7.ts

---
+++ javascript-basics/exercice7.ts
@@ -0,0 +1,7 @@
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+
+function myFunction(a: string): string {
+  return a.substring(0, 3);
+}
