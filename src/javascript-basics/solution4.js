d5cee4c75e2e20bb52d89b0e71b0d6ccd2a4d226
gloirekiba <gloirekiba21@gmail.com>
2022-07-18 11:31:56 +0200

    ♻️ refactor code

--- src/javascript-basics/solution4.js
+++ src/javascript-basics/solution4.js
@@ -1,6 +1,6 @@
 // Write a function that takes a string (a) and a number (n) as argument
 // Return the nth character of 'a'
 
-function myFunction(a: string, n: number): string {
+function myFunction(a, n) {
   return a[n - 1];
 }
